789dce9ed8757c8feb6baac2f608ed80f9856822
Mr. T <tholkingk@smtexas.org>
2016-10-24 12:19:34 -0500

    Starter code

---
+++ TweetTester/src/tweettester/TweetTester.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tweettester;
+
+import java.util.Scanner;
+import java.lang.Math; 
+/**
+ * @class  AP CSA
+ * @author INSERT YOUR NAME HERE
+ */
+public class TweetTester {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner (System.in);
+         String tweet;
+         
+    }
+    
+}
